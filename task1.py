3951bdbb9e7f4afb3c1a7bf86d687c514891533a
Nadia <56296447+nadkkka@users.noreply.github.com>
2020-03-16 15:57:25 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,11 +1,5 @@
 def rectangle_area(a, b):
-    """
-    Calculates the area of a rectangle given its side lengths
+    if a <= 0 or b <= 0:
+        raise ValueError("if either number was negative")
 
-    :param a: first side of the rectangle
-    :param b: second side of the rectangle
-    :return: area of the rectangle
-    :raises ValueError: if either number was negative
-    """
-    
-    pass
+    return a*b
